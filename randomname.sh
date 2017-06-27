82eb644def4c2177171930631aef609676a48d0b
George Hotelling <george.hotelling@automattic.com>
2017-06-27 16:06:31 -0400

    Using NVM for node

--- randomname.sh
+++ randomname.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source ~/.nvm/nvm.sh
 lastline=`tail -1 randomized_anagrams.txt`
 # echo $lastline
 node changename.js $lastline && truncate -s -"$(tail -n1 randomized_anagrams.txt | wc -c)" randomized_anagrams.txt
